663ed1fe67150c8017fb77e908a2c938159531de
jonatan <jonatan5524@gmail.com>
2026-03-14 12:31:17 +0200

    fix

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -6,12 +6,10 @@ module.exports = {
       instances: "max",
       exec_mode: "cluster",
       env: {
-        NODE_ENV: "development",
-        PORT: 3000,
+        NODE_ENV: "development"
       },
       env_production: {
-        NODE_ENV: "production",
-        PORT: 3000,
+        NODE_ENV: "production"
       },
       error_file: "./logs/pm2-error.log",
       out_file: "./logs/pm2-out.log",
